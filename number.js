a5d24076bdb691da47cff532238b6fe82eb6fdf8
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-15 22:04:02 +0600

    number data type  conversion

---
+++ number.js
@@ -0,0 +1,26 @@
+let orangePrice = 30;
+// console.log(orangePrice);
+
+let mangoPrice = 25.7;
+// console.log(mangoPrice);
+
+let lemonPrice = 40;
+// console.log(lemonPrice);
+
+let fruitsPrice = orangePrice + lemonPrice;
+// console.log(fruitsPrice);
+
+let bangla = 70;
+let english = "40";
+
+let englishInteger = parseInt(english);
+let totalSubject = bangla + englishInteger;
+
+// console.log(totalSubject);
+
+let math = 88;
+let economic = "82.5";
+
+let twoSub = math + parseFloat(economic);
+
+console.log(twoSub);
