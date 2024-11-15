a8ed1c5cd8a560cc9d474188e1ae7320f8a46b90
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-15 21:09:06 +0600

    3 common javascript primitive data type practice

---
+++ valuetype.js
@@ -0,0 +1,20 @@
+// number data type
+let number = 25;
+console.log(number);
+// how to check data type
+let numberType = typeof number;
+console.log(numberType);
+
+// string data type
+let name = "Sonatan Paul";
+console.log(name);
+// how to check data type
+let stringType = typeof name;
+console.log(stringType);
+
+// boolean data type
+let isSuccess = false;
+console.log(isSuccess);
+// how to check data type
+let booleanType = typeof isSuccess;
+console.log(booleanType);
