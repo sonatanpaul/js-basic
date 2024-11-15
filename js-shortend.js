29793abbfcf25bdd4b801832b5ee4d733bfee897
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-15 23:16:17 +0600

    javascript some shorthand practice

---
+++ js-shortend.js
@@ -0,0 +1,31 @@
+let number = 350;
+console.log(number); //ans: 350;
+number = number + 50;
+console.log(number); // ans: 400;
+
+// Addition Shorthand
+number += 100;
+console.log(number); // ans:500
+
+let multiplication = 50;
+console.log(multiplication); // ans: 50;
+
+multiplication = multiplication * 10;
+console.log(multiplication); // ans: 500;
+
+// multiplication Shorthand
+
+multiplication *= 10;
+console.log(multiplication); // 500 * 10 ans : 5000;
+
+// division
+
+let division = 50;
+division /= 5;
+console.log(division);
+
+// Subtraction
+
+let subtraction = 100;
+subtraction -= 50;
+console.log(subtraction);
