9b2e61bbd07a18ec4293a3808a938a6e2a72bb54
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-15 21:24:59 +0600

    javascript variable nameing

---
+++ js-naming.js
@@ -0,0 +1,23 @@
+// 1.No keyword in variable name .
+
+// wrong way 
+let false = 45; // 
+console.log(false); // ans: error .
+
+// Correct Way 
+let isFalse = 45;
+console.log(isFalse); // ans:45 
+
+// 2. No space or gap in variable name
+
+//Wrong Way
+let is happy = false;
+console.log(is happy); // ans:error
+
+// Correct Way 
+let isHappy = false;
+console.log(isHappy); // ans: false
+
+
+
+
